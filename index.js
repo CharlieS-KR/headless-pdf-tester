2cdf8a6ceb00c5380338428f40ac827405a11792
SparksFullStack <csparks@antioch.edu>
2019-06-12 16:39:35 -0400

    successfully generated both an image and a PDF out of a webpage

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-alert("working");
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://127.0.0.1:5500/index.html');
+    await page.pdf({ path: './output/test.pdf' });
+
+    await browser.close();
+})();

119ade2f58bbe2fdd96bf330e638a13dde71c911
SparksFullStack <csparks@antioch.edu>
2019-06-13 11:09:30 -0400

    fixed issue with not being able to use second route--both routes are fully functional now

--- server/genPdf.js
+++ server/genPdf.js
@@ -1,15 +1,15 @@
 const puppeteer = require('puppeteer');
-const iPhone = puppeteer.devices['iPhone X'];
 
-function genPdf(url, filePath = './output/output.pdf', format = 'A4', deviceType) {
-    (async () => {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        if (deviceType) await page.emulate(deviceType);
-        await page.goto(url);
-        await page.pdf({ path: filePath, format: format });
-        await browser.close();
-    })();
+async function genPdf(url, filePath = './output/output.pdf', format = 'A4', deviceType) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    if (deviceType) {
+        const device = puppeteer.devices['iPhone X'];
+        await page.emulate(device);
+    }
+    await page.goto(url);
+    await page.pdf({ path: filePath, format: format });
+    await browser.close();
 }
 
 module.exports = genPdf;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const port = process.env.PORT || 3001;
 
-const genPdf = require('./genPdf'); // PARAMETERS: (url, filePath, deviceType, format)
+const genPdf = require('./genPdf'); // PARAMETERS: (url, filePath, format, deviceType)
 
 const server = express();
 server.use(express.json());
@@ -9,10 +9,18 @@ server.use(express.json());
 server.post('/generate-pdf', (req, res) => {
     const { url, filePath } = req.body;
     const fullFilePath = __dirname + filePath;
-
-    genPdf(url, fullFilePath);
-    res.sendFile(fullFilePath);
+    genPdf(url, fullFilePath)
+        .then(() => res.sendFile(fullFilePath))
+        .catch((err) => console.log("--generate-pdf: There was an error generating the PDF", err));
 });
+
+server.post('/generate-pdf/more-details', (req, res) => {
+    const { url, filePath, format, deviceType } = req.body;
+    const fullFilePath = __dirname + filePath;
+    genPdf(url, fullFilePath, format, deviceType)
+        .then(() => res.sendFile(fullFilePath))
+        .catch((err) => console.log("--generate-pdf/more-details: There was an error generating the PDF", err));
+})
 
 server.listen(port, () => console.log(`The server is listening on port ${port}`));
 server.listen()
